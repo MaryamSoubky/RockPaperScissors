69deb8841f0acebdd946042de5a75d1bdba4e8be
Maryam Soubky <156623912+MaryamSoubky@users.noreply.github.com>
2026-03-28 02:27:40 +0200

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,16 @@
+# Screen
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+
+# Colors
+BG_COLOR = "#5d4e60"
+TEXT_COLOR = "#D4B2D8"
+HOVER_TEXT_COLOR = "#EFCEFA"
+NORMAL_BUTTON_COLOR = (168, 143, 172, 180)
+HOVER_BUTTON_COLOR = (239, 206, 250, 180)
+
+# Font
+FONT_PATH = "images/font.ttf"
+
+# Game
+CHOICES_NAMES = {'r': 'Rock', 'p': 'Paper', 's': 'Scissors'}
